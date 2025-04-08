d2445c3d7c621dcef5cded951a53500bdc02f0dd
Bombardier256 <4477617@gmail.com>
2025-04-08 18:36:23 +0300

    Changed back to Serializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import Movie
 
 
-class MovieSerializer(serializers.ModelSerializer):
+class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=True, max_length=250)
     description = serializers.CharField(required=True)
